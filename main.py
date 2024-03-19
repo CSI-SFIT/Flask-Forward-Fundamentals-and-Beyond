6341e276b2869c865accc4a4967f5d110cf3610f
Reuben <136352370+ReubenMatrix@users.noreply.github.com>
2024-03-19 23:48:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ app.config.update(
             MAIL_PORT = 465,
             MAIL_USE_TLS = False,
             MAIL_USE_SSL = True,
-            MAIL_USERNAME = '',
-            MAIL_PASSWORD = ''
+            MAIL_USERNAME = 'SFIT_EMAIL_ID',
+            MAIL_PASSWORD = 'PASSWORD'
         )
 mail = Mail(app)
 
